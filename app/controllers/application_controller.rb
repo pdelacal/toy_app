5dc3e0a2ee7805c7b6e05d4b42d88097ff37ce3d
Philip de la Cal <pdelacal@gmail.com>
2017-02-24 19:10:30 -0500

    added microposts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "hello, world!"
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
